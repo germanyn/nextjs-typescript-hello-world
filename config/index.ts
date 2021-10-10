cd8185241bba961efc6f34bed1897fce10fb3d2e
Germano Gascho <germanogascho@gmail.com>
2021-10-10 18:09:50 -0300

    deploy config

--- config/index.ts
+++ config/index.ts
@@ -2,4 +2,4 @@ export const dev = process.env.NODE_ENV !== 'production'
 
 export const SERVER_URL = dev
     ? 'http://localhost:3000'
-    : 'https://mywebsite.com'
+    : 'https://germanyn.github.io/nextjs-typescript-hello-world'
